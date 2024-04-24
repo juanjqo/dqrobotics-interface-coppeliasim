1971a748ab6787c0364537bc9d5cc15693ff2d02
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2024-04-23 21:50:16 -0500

    Updated the cmake file to include automatically BOOST

--- dependencies.cmake
+++ dependencies.cmake
@@ -22,6 +22,44 @@ endif()
 
 
 
+set(BOOST_INCLUDE_LIBRARIES thread format filesystem system program_options)
+set(BOOST_ENABLE_CMAKE ON)
+
+#-----This works-------------------------------------#
+# Download and extract the boost library from GitHub
+#message(STATUS "Downloading and extracting boost library sources. This will take some time...")
+#Set(FETCHCONTENT_QUIET FALSE)
+#include(FetchContent)
+#FetchContent_Declare(
+#  Boost
+#  GIT_REPOSITORY https://github.com/boostorg/boost.git
+#  GIT_TAG boost-1.84.0
+#  USES_TERMINAL_DOWNLOAD TRUE
+#  GIT_PROGRESS TRUE
+#  GIT_SHALLOW TRUE
+#)
+#FetchContent_MakeAvailable(Boost)
+#-----------------------------------------------
+
+
+
+# Add boost lib sources
+set(BOOST_INCLUDE_LIBRARIES thread format filesystem system program_options)
+set(BOOST_ENABLE_CMAKE ON)
+
+# Download and extract the boost library from GitHub
+message(STATUS "Downloading and extracting boost library sources. This will take some time...")
+include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE) # Needed to print downloading progress
+FetchContent_Declare(
+    Boost
+    #URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.7z # downloading a zip release speeds up the download
+    URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.gz
+    USES_TERMINAL_DOWNLOAD TRUE
+    GIT_PROGRESS TRUE
+    DOWNLOAD_NO_EXTRACT FALSE
+)
+FetchContent_MakeAvailable(Boost)
 
 
 
