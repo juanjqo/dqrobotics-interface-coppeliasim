064711938c61cdceac57950a04b2682d549072e1
Juancho <juanjqogm@mgmail.com>
2024-11-16 22:06:55 +0000

    Boost is now a requirement for Ubuntu users

--- dependencies.cmake
+++ dependencies.cmake
@@ -11,18 +11,28 @@ if(UNIX AND NOT APPLE)
     #find_package(cppzmq REQUIRED)
     #find_package(ZeroMQ REQUIRED)
     ADD_COMPILE_OPTIONS(-Werror=return-type -Wall -Wextra -Wmissing-declarations -Wredundant-decls -Woverloaded-virtual)
+    
+    # libboost-all-dev 
 
-    include(FetchContent)
-    set(CPPZMQ_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-    FetchContent_Declare(cppzmq
-        GIT_REPOSITORY https://github.com/zeromq/cppzmq
-        SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/cppzmq
+    find_package(Boost 1.74.0 COMPONENTS filesystem format options)
+    include_directories(${Boost_INCLUDE_DIRS})
+    set(CUSTOM_BOOST_COMPONENTS
+        ${Boost_PROGRAM_FILESYTEM_LIBRARY}
+        ${Boost_PROGRAM_FORMAT_LIBRARY}
+        ${Boost_PROGRAM_OPTIONS_LIBRARY}
     )
-    FetchContent_GetProperties(cppzmq)
-    if(NOT cppzmq_POPULATED)
-        FetchContent_Populate(cppzmq)
-        add_subdirectory(${cppzmq_SOURCE_DIR} ${cppzmq_BINARY_DIR} EXCLUDE_FROM_ALL)
-    endif()
+
+    #include(FetchContent)
+    #set(CPPZMQ_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    #FetchContent_Declare(cppzmq
+    #    GIT_REPOSITORY https://github.com/zeromq/cppzmq
+    #    SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/cppzmq
+    #)
+    #FetchContent_GetProperties(cppzmq)
+    #if(NOT cppzmq_POPULATED)
+    #    FetchContent_Populate(cppzmq)
+    #    add_subdirectory(${cppzmq_SOURCE_DIR} ${cppzmq_BINARY_DIR} EXCLUDE_FROM_ALL)
+    #endif()
 
 endif()
 
@@ -43,6 +53,24 @@ if(APPLE) #APPLE
 
     find_package(cppzmq REQUIRED)
     find_package(ZeroMQ REQUIRED)
+
+    find_package(Boost)
+    if(Boost_FOUND)
+        #if (Boost_VERSION_MAJOR LESS_EQUAL 1 AND Boost_VERSION_MINOR LESS_EQUAL 81)
+                include_directories(${Boost_INCLUDE_DIRS})
+                message(AUTHOR_WARNING "Local Boost ${Boost_VERSION_MAJOR}.${Boost_VERSION_MINOR}.${Boost_VERSION_COUNT} found!")
+                set(CUSTOM_BOOST_COMPONENTS
+                    ${Boost_PROGRAM_FILESYTEM_LIBRARY}
+                    ${Boost_PROGRAM_FORMAT_LIBRARY}
+                    ${Boost_PROGRAM_OPTIONS_LIBRARY}
+                    )
+        #else()
+        #    message(AUTHOR_WARNING "Local Boost ${Boost_VERSION_MAJOR}.${Boost_VERSION_MINOR}.${Boost_VERSION_COUNT} is not compatible. I'm going to download a compatible one!")
+        #endif()
+    else()
+        message(AUTHOR_WARNING "Local Boost not found. I'm going to download it!")
+        include(boost_dependencies.cmake)
+    endif()
 
 endif()
 
@@ -65,23 +93,7 @@ if(WIN32)
 endif()
 
 
-find_package(Boost)
-if(Boost_FOUND)
-    #if (Boost_VERSION_MAJOR LESS_EQUAL 1 AND Boost_VERSION_MINOR LESS_EQUAL 81)
-            include_directories(${Boost_INCLUDE_DIRS})
-            message(AUTHOR_WARNING "Local Boost ${Boost_VERSION_MAJOR}.${Boost_VERSION_MINOR}.${Boost_VERSION_COUNT} found!")
-            set(CUSTOM_BOOST_COMPONENTS
-                ${Boost_PROGRAM_FILESYTEM_LIBRARY}
-                ${Boost_PROGRAM_FORMAT_LIBRARY}
-                ${Boost_PROGRAM_OPTIONS_LIBRARY}
-                )
-    #else()
-    #    message(AUTHOR_WARNING "Local Boost ${Boost_VERSION_MAJOR}.${Boost_VERSION_MINOR}.${Boost_VERSION_COUNT} is not compatible. I'm going to download a compatible one!")
-    #endif()
-else()
-    message(AUTHOR_WARNING "Local Boost not found. I'm going to download it!")
-    include(boost_dependencies.cmake)
-endif()
+
 
 
 
@@ -101,15 +113,15 @@ message(STATUS "Dependencies ready!")
 
 
 
-include(FetchContent)
-FetchContent_Declare(
-  googletest
-  # Specify the commit you depend on and update it regularly.
-  #URL https://github.com/google/googletest/archive/5376968f6948923e2411081fd9372e71a59d8e77.zip
-  URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.tar.gz
-)
+#include(FetchContent)
+#FetchContent_Declare(
+#  googletest
+#  # Specify the commit you depend on and update it regularly.
+#  #URL https://github.com/google/googletest/archive/5376968f6948923e2411081fd9372e71a59d8e77.zip
+#  URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.tar.gz
+#)
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
+#set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+#FetchContent_MakeAvailable(googletest)
 
